edac4cbef043abd5e44ee8dd6fe4ad3b75171d89
Robert King <varitonium@gmail.com>
2026-05-20 16:49:44 +0200

    TCX-4: dev-NCNM class impl

---
+++ test/test_core.cpp
@@ -0,0 +1,16 @@
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+
+#include <corety.h>
+#include <ncnm.h>
+
+template <typename T> using Default = core::NCNM<T>;
+
+struct Sample : public Default<Sample> {};
+
+TEST_CASE("Non copyble non moveable class") {
+  static_assert(topology::NonCopyableNonMovable<Sample>);
+  CHECK(true);
+}
