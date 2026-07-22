9c0c498f1a214ce86b20d51edf2daf0a7bd18299
Robert King <varitonium@gmail.com>
2026-07-23 00:14:42 +0200

    TCX-21: dev-micro macro impl

---
+++ include/obs/runtime.h
@@ -0,0 +1,29 @@
+
+#ifndef RUNTIME_H
+#define RUNTIME_H
+
+#include <xclock.h>
+#include <fmt/core.h>
+
+namespace obs{
+
+#define MICRO_CLOCK_RUNTIME_MAIN                                        \
+    static int clock_runtime_main(                                      \
+        int argc, char** argv, obs::micro_c& clock);                    \
+                                                                        \
+    int main(int argc, char** argv) {                                   \
+        if (argc == 1) {                                                \
+            fmt::println("missing argument");                           \
+            return EXIT_FAILURE;                                        \
+        }                                                               \
+                                                                        \
+        obs::micro_c clock;                                             \
+        return clock_runtime_main(argc, argv, clock);                   \
+    }                                                                   \
+                                                                        \
+    static int clock_runtime_main(                                      \
+        int argc, char** argv, [[maybe_unused]] obs::micro_c& clock)
+
+} //namespace obs
+
+#endif // RUNTIME_H
