b90e2e3196c0bb6c51e027bf61b4eecfbcc1445b
Robert King <varitonium@gmail.com>
2026-08-10 20:44:29 +0200

    TCX-34: upe-amending of second clock test

--- test/test_obs_runtime.cpp
+++ test/test_obs_runtime.cpp
@@ -11,9 +11,8 @@ TEST_CASE("clock behavior") { CHECK(1 + 1 == 2); }
 CLOCK_RUNTIME_MAIN {
   clock();
   clock();
-  /*todo change the interface used to extract the elapsed time*/
-  const auto time = clock.elapsed_to_str();
-  fmt::println("{}", time);
+  const auto time = clock.elapsed();
+  fmt::println("{} s", time);
   doctest::Context context;
   context.applyCommandLine(argc, argv);
   return context.run();
